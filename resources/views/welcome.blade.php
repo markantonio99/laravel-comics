4372ba71cfe2381d45b26c6525e38bca31933cd3
“markantonio99” <markomakkia@gmail.com>
2023-04-11 15:53:08 +0200

    fix. folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <div class="min-vh-100 py-5 d-flex flex-column justify-content-center">
+            {{-- <div class="min-vh-100 py-5 d-flex flex-column justify-content-center">
                 <div class="logo_laravel">
                     <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-25">
                         <g clip-path="url(#clip0)" fill="#EF3B2D">
@@ -19,7 +19,9 @@
 
                 <p class="col-md-8 fs-4">This a preset package with Bootstrap 5 views for laravel projects including laravel breeze/blade. It works from laravel 9.x to the latest release 10.x</p>
                 <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg" type="button">Documentation</a>
-            </div>
+            </div> --}}
+
+           
         </div>
     </div>
 
